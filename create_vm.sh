59a94a61e10e5c4370fa35191c538d8d89335469
Ved Patel <vedmpatel@gmail.com>
2023-05-05 16:28:54 -0700

    remove mac addr option

--- create_vm.sh
+++ create_vm.sh
@@ -6,17 +6,15 @@ PE_SIZE="80"
 
 function usage() {
     echo "Usage"
-    echo -e "\tbash $0 <vm-type> <vm-name> <mac-addr-mgmt> <vnc-port>"
+    echo -e "\tbash $0 <vm-type> <vm-name> <vnc-port>"
     echo ""
     echo -e "\t   1. vm-type : <pe | ce> (pe = provider edge, ce = customer edge)"
     echo -e "\t   2. vm-name : <name>"
-    echo -e "\t   3. mac-addr-mgmt : 52:54:00:xx:xx:xx"
     echo -e "\t   4. vnc-port : <port> (port >= 5900)"
 }
 
 function create_pe() {
     VM_NAME=${1}
-    MAC_ADDR=${2}
     VNC_PORT=${3}
     sudo virt-install \
         --virt-type kvm \
@@ -27,7 +25,7 @@ function create_pe() {
         --graphics vnc,listen=0.0.0.0,port=${VNC_PORT} \
         --cdrom /var/lib/libvirt/boot/ubuntu-22.04.2-live-server-amd64.iso \
         --disk path=/var/lib/libvirt/images/${VM_NAME}.qcow2,size=80,bus=virtio,format=qcow2 \
-        --network bridge=br-mgmt1,model=virtio,mac=${MAC_ADDR} \
+        --network bridge=br-mgmt1,model=virtio \
         --network bridge=br1,model=virtio \
         --network bridge=br2,model=virtio \
         --network bridge=br-iso1,model=virtio \
@@ -37,7 +35,6 @@ function create_pe() {
 
 function create_ce() {
     VM_NAME=${1}
-    MAC_ADDR=${2}
     VNC_PORT=${3}
     sudo virt-install \
         --virt-type kvm \
@@ -48,7 +45,7 @@ function create_ce() {
         --graphics vnc,listen=0.0.0.0,port=${VNC_PORT} \
         --cdrom /var/lib/libvirt/boot/ubuntu-22.04.2-live-server-amd64.iso \
         --disk path=/var/lib/libvirt/images/${VM_NAME}.qcow2,size=40,bus=virtio,format=qcow2 \
-        --network bridge=br-mgmt1,model=virtio,mac=${MAC_ADDR} \
+        --network bridge=br-mgmt1,model=virtio \
         --network bridge=br-iso1,model=virtio \
         --network bridge=br-iso2,model=virtio \
         --network bridge=br1,model=virtio \
@@ -57,7 +54,6 @@ function create_ce() {
 
 function create_dev() {
     VM_NAME=${1}
-    MAC_ADDR=${2}
     VNC_PORT=${3}
     sudo virt-install \
         --virt-type kvm \
@@ -68,7 +64,7 @@ function create_dev() {
         --graphics vnc,listen=0.0.0.0,port=${VNC_PORT} \
         --cdrom /var/lib/libvirt/boot/ubuntu-22.04.2-live-server-amd64.iso \
         --disk path=/var/lib/libvirt/images/${VM_NAME}.qcow2,size=300,bus=virtio,format=qcow2 \
-        --network bridge=br-mgmt1,model=virtio,mac=${MAC_ADDR} \
+        --network bridge=br-mgmt1,model=virtio \
         --network bridge=br1,model=virtio \
         --network bridge=br2,model=virtio \
         --noautoconsole
@@ -80,7 +76,7 @@ function delete_vm() {
     virsh undefine ${VM_NAME} --remove-all-storage
 }
 
-if [[ "$#" -ne 4 ]]; then
+if [[ "$#" -ne 3 ]]; then
     usage
     exit
 fi
@@ -88,15 +84,15 @@ fi
 case $1 in
     pe)
         shift
-        create_pe ${1} ${2} ${3}
+        create_pe ${1} ${2}
         ;;
     ce)
         shift
-        create_ce ${1} ${2} ${3}
+        create_ce ${1} ${2}
         ;;
     dev)
         shift
-        create_dev ${1} ${2} ${3}
+        create_dev ${1} ${2}
         ;;
     *)
         usage
