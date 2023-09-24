9b15fb1c69001870e3fddbb49ab757d82f318294
Ved Patel <vedmpatel@gmail.com>
2023-09-24 00:39:58 -0700

    Add Config Validations
    1. Validate networks on creation
       - Check if interface/bridge exists with the same name
       - Check if provided v4 and v6 subnet collides with existing networks
    2. Validate vms on creation
       - Check if vm config has valid fields
       - Check for either flavor or vcpus, disk and ram
       - Check if provided network exists
       - Check if provided IPs belongs to configured subnets
    3. Update topologies to version 2
    4. Reorganize code and clean up
    
    Closes #18

--- deployer/globals.py
+++ deployer/globals.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 OP_CREATE = 1
 OP_DELETE = 2
-
 OP = OP_CREATE
+
 DRY_RUN = False
 NO_NETWORK = False
 NO_VM = False
-RECREATE_NW = False
-PRINT_NETWORK = False
-PRINT_VM = False
+
+NETWORK_TYPES = ["nat", "isolated", "management"]
+VM_FLAVORS = ["pe", "ce", "dev"]
 
 LIBVIRT_BASE_PATH = Path('/var/lib/libvirt')
 LIBVIRT_IMAGES = LIBVIRT_BASE_PATH.joinpath('images')
