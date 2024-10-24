295491d5552cc12745dc21df5eae123730463d4a
Ved Patel <vedmpatel@gmail.com>
2024-10-24 14:51:21 -0700

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fhand:
 
 setuptools.setup(
     name="topology-deployer",
-    version="0.1.0",
+    version="0.1.1",
     author="Ved Patel",
     description=("A python tool to deploy topology "
                 "defined in a json config"),
