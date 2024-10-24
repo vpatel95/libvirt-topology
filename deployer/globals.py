c34664bffc80a711ff0e4264a3893230df9af5a0
Ved Patel <vedmpatel@gmail.com>
2024-10-24 14:51:20 -0700

    Add support to bring up Rocky Linux VMs
    Closes #15

--- deployer/globals.py
+++ deployer/globals.py
@@ -17,5 +17,9 @@ LIBVIRT_TEMPLATES = LIBVIRT_IMAGES.joinpath('templates')
 LIBVIRT_QEMU_BASE = Path('/etc/libvirt/qemu')
 LIBVIRT_QEMU_NW = LIBVIRT_QEMU_BASE.joinpath('networks')
 
+BASE_OS = "ubuntu"
 UBUNTU_TEMPLATE = LIBVIRT_TEMPLATES.joinpath('ubuntu-22.04-server.qcow2')
+ROCKY_TEMPLATE = LIBVIRT_TEMPLATES.joinpath('rocky-8.qcow2')
+OS_IMAGE_TEMPLATE = UBUNTU_TEMPLATE
+
 NAT_NW_BASE = Path.home().joinpath('.topology/bridges')
